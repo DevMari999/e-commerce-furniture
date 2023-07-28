eecd263b8638300843d5bfa9c7645af6e8bf5c24
DvlprMrn <rplc.999@outlook.com>
2023-07-27 20:26:09 -0400

    commit 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import ProductDetails from "./components/ProductDetails/ProductDetails";
+import ShoppingCartPage from "./components/ShoppingCartPage/ShoppingCartPage";
+import CheckoutPage from "./components/CheckoutPage/CheckoutPage";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => {
+    return (
+        <Router>
+            <div>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/product/:productId" element={<ProductDetails />} />
+                    <Route path="/cart" element={<ShoppingCartPage />} />
+                    <Route path="/checkout" element={<CheckoutPage />} />
+                </Routes>
+            </div>
+        </Router>
+    );
+};
 
 export default App;
+
