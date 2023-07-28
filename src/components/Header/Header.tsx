cc9bc38fc804e0615b1e506c31287fa16b58c54e
DvlprMrn <rplc.999@outlook.com>
2023-07-28 00:45:49 -0400

    commit 1

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
-import "./Header.css";
+import { Link } from 'react-router-dom';
+import './Header.css';
 
-const Header = () => {
+const Header: React.FC = () => {
     return (
-        <div>
-
-        </div>
+        <header className="header">
+            <nav className="container">
+                <Link to="/" className="logo">FW</Link>
+                <div className="nav-elements">
+                    <div className="main-nav-item">
+                        <Link to="/">Home</Link>
+                    </div>
+                    <div className="divider"></div>
+                    <div className="main-nav-item">
+                        <Link to="/checkout">Shop All</Link>
+                    </div>
+                    <div className="divider"></div>
+                    <div className="main-nav-item">
+                        <Link to="/cart">Cart</Link>
+                    </div>
+                </div>
+            </nav>
+        </header>
     );
 };
 
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import "./Home.css";
+import background from "../../assets/background.png";
 
 const Home = () => {
     return (
-        <div>
+        <div className="main-home">
+            <div className="background-home">
+                <img src={background}/>
+            </div>
+            <div className="header-home">
+                <h1>FURNITURE WORLD</h1>
+                <h2>WHERE STYLE MEETS FUNCTIONALITY</h2>
+            </div>
             
         </div>
     );
